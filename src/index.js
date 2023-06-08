02ba94b1ddee3af599a11fbad4dedbbca5c37024
Tae-kyun Kim <ktk03115@gmail.com>
2023-06-09 03:36:15 +0900

    Change Router type

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import App from './App';
 import ScrollToTop from './components/ScrollToTop';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router 
-    path="/considering-2023diaf-web" 
-    basename={process.env.PUBLIC_URL}>
+  <Router>
   <ScrollToTop />
     <App />
   </Router>
