8242cf669ec75e92b772b9ccda246f7c4ec2ad9c
Tae-kyun Kim <ktk03115@gmail.com>
2023-06-09 03:06:47 +0900

    Fix Router type
    HashRouter to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import ScrollToTop from './components/ScrollToTop';
 import reportWebVitals from './reportWebVitals';
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <Router>
   <ScrollToTop />
     <App />
-  </HashRouter>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
